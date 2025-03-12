4b05b75e30a967070b45b342e44d04d72427a139
Sumit Guha <sumit03guha@gmail.com>
2025-03-12 22:56:27 +0530

    Implement basic Axum server with a Hello World route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = app();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!(
+        "Server up and running on {}",
+        listener.local_addr().unwrap()
+    );
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(|| async { "Hello World" }))
 }
