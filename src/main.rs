f8cc711e3ad35148fdc6a037502ab0c4711b4209
Sumit Guha <sumit03guha@gmail.com>
2025-03-12 23:52:59 +0530

    Refactor Axum server to initialize MongoDB connection and pass identity collection to router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use axum::{
     Json, Router,
     extract::Path,
@@ -5,6 +7,8 @@ use axum::{
     response::IntoResponse,
     routing::{get, post},
 };
+use mongodb::{Client, Collection, Database, bson::Document};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
@@ -15,23 +19,39 @@ struct Identity {
 
 #[tokio::main]
 async fn main() {
-    let app = app();
+    let db: Database = init_db().await;
+    let identity_collection: Collection<Document> = init_identity_collection(db);
+    let app: Router = app(identity_collection);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!(
         "Server up and running on {}",
         listener.local_addr().unwrap()
     );
+
     axum::serve(listener, app).await.unwrap();
 }
 
-fn app() -> Router {
-    let crud_router = crud_router();
+fn app(collection: Collection<Document>) -> Router {
+    let crud_router = crud_router(collection);
     Router::new()
         .route("/", get(|| async { "Hello World" }))
         .merge(crud_router)
 }
 
-fn crud_router() -> Router {
+async fn init_db() -> Database {
+    let client: Client = Client::with_uri_str("mongodb://localhost:27017/")
+        .await
+        .unwrap();
+
+    client.database("restful_axum")
+}
+
+fn init_identity_collection(database: Database) -> Collection<Document> {
+    database.collection::<Document>("identity")
+}
+
+fn crud_router(collection: Collection<Document>) -> Router {
     Router::new()
         .route("/identity", post(create_identity).get(get_all_identities))
         .route(
@@ -40,6 +60,7 @@ fn crud_router() -> Router {
                 .patch(update_identity)
                 .delete(delete_identity),
         )
+        .with_state(Arc::new(collection))
 }
 
 async fn create_identity(Json(identity): Json<Identity>) -> impl IntoResponse {
